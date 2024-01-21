712acf6b305970843b0ca3dc45a20bd6093dd212
AnnaLinden <linden.a.anna@gmail.com>
2024-01-21 17:01:59 +0200

    city map

--- markdown/assignments/FACTORY_METHOD/CityMap.java
+++ markdown/assignments/FACTORY_METHOD/CityMap.java
@@ -5,15 +5,37 @@ import java.util.Random;
 
 public class CityMap implements Map {
 
+    private Tile[][] grid;
+    private int size = 5;
+
+    public CityMap() {
+
+        grid = new Tile[size][size];
+        generateMap();
+    }
+
     @Override
     public Tile createTile() {
         List<Tile> tiles = List.of(new RoadTile(), new BuildingTile(), new ForestTile());
         return tiles.get(new Random().nextInt(tiles.size()));
     }
 
+    private void generateMap() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                grid[i][j] = createTile();
+            }
+        }
+    }
+
     @Override
     public void diaplay() {
-        System.out.println("City Map");
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(grid[i][j].getCharacter() + " ");
+            }
+            System.out.println();
+        }
     }
 
 }
